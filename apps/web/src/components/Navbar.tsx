4826660a5eaef9b403d7dd0fdf81d55eb6908186
Satyajit799 <satyajitmohanty431@gmail.com>
2026-09-21 23:52:56 +0530

    Make student UI responsive

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -2,37 +2,14 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav
-      style={{
-        padding: "15px 30px",
-        borderBottom: "1px solid #ddd",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-      }}
-    >
-      <Link
-        to="/"
-        style={{
-          textDecoration: "none",
-          fontSize: "22px",
-          fontWeight: "bold",
-          color: "#111",
-        }}
-      >
+    <nav className="navbar">
+      <Link to="/" className="navbar-brand">
         College Canteen
       </Link>
 
-      <div
-        style={{
-          display: "flex",
-          gap: "20px",
-        }}
-      >
+      <div className="navbar-links">
         <Link to="/">Dashboard</Link>
-
         <Link to="/book">Book Meal</Link>
-
         <Link to="/bookings">My Bookings</Link>
       </div>
     </nav>
